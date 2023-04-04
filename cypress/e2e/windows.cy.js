081937b42fd3aade135d9ad6b9243e1c8bd42c5d
Priyasi <priyasi.singh@axelerant.com>
2023-04-05 00:26:49 +0530

    day 2_1

--- cypress/e2e/windows.cy.js
+++ cypress/e2e/windows.cy.js
@@ -5,7 +5,7 @@ describe('Access the windows and verify the functionality of the three window op
 
     beforeEach(() => {
         cy.visit('https://demo.automationtesting.in/')
-        cy.viewport('macbook-16')
+        // cy.viewport('macbook-16')
         cy.get('#email').click().type('priyasi.singh@axelerant.com')
         cy.get('#enterimg').click()
         cy.get('h2').should('have.text', 'Register')
@@ -15,11 +15,24 @@ describe('Access the windows and verify the functionality of the three window op
 
     it('Verify new tab window', () => {
 
+        Cypress.config('pageLoadTimeout', 10000);
+
         cy.get("[href='#Tabbed']").click()
 
-        cy.get("[href='http://www.selenium.dev']")
-            .invoke('removeAttr', 'target')
-            .click()
+
+        //remove the target via dev tools, the link properly opens in the same browser tab. 
+        //But this doesn't happen when I use invoke('removeAttr', 'target')
+        // cy.get("[href='http://www.selenium.dev']")
+        //     .invoke("removeAttr", "target")
+        //     .click()
+
+        // cy.get("[href='http://www.selenium.dev']").then(link => {
+        //     cy.request(link.prop('href')).its('status').should('eq', 200)
+        // })
+
+        cy.get("[href='http://www.selenium.dev']").invoke("removeAttr", "target")
+        cy.get("[href='http://www.selenium.dev']").click()
+        cy.location().then(yieldedObject => cy.log(yieldedObject.href))
 
         cy.get('h1').should('have.text', 'Selenium automates browsers')
 
@@ -27,10 +40,29 @@ describe('Access the windows and verify the functionality of the three window op
     })
 
     it('Verify new seperate window', () => {
+        cy.get("[href='#Seperate']").click()
+
+        cy.window().then(function (win) {
+            cy.stub(win, 'open').returns(url => {
+                win.location.href = 'https://www.selenium.dev/';
+            }).as("popup")
+        })
+
+        cy.get('.btn-primary').click()
+        cy.get('@popup')
+            .should("be.called")
+        cy.get('h1')
+            .should('have.text', 'Selenium')
 
     })
 
     it('Verify multiple window box', () => {
+
+        cy.get("[href='#Multiple']").click()
+
+        cy.get('#Multiple > .btn')
+            .invoke("removeAttr", "onclick")
+            .click()
 
     })
 
